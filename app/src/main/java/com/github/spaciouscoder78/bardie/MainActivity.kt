6f7a879f05183003664dc830664213b4e6d823d2
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-11-23 19:20:21 +0530

    built the basic version

--- app/src/main/java/com/github/spaciouscoder78/bardie/MainActivity.kt
+++ app/src/main/java/com/github/spaciouscoder78/bardie/MainActivity.kt
@@ -2,10 +2,32 @@ package com.github.spaciouscoder78.bardie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    //creating a webview object
+    private lateinit var webView:WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //looking for the webview id from android main
+        webView = findViewById(R.id.webview)
+        //transferring the control to webviewclient
+        webView.webViewClient = WebViewClient()
+        //loading the url
+        webView.loadUrl("https://bard.google.com/chat")
+        //enabling javascript so it actually works
+        webView.settings.javaScriptEnabled=true
+        //adding zoom support
+        webView.settings.setSupportZoom(true)
+
+    }
+    override fun onBackPressed(){
+        //allowing user to go back
+        if(webView.canGoBack())
+            webView.goBack()
+        else
+            super.onBackPressed()
     }
 }
