62b4e8c302604944349401b8bd6d92dea9cb946b
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-11-23 18:34:37 +0530

    initial commit

---
+++ app/src/main/java/com/github/spaciouscoder78/bardie/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.spaciouscoder78.bardie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
